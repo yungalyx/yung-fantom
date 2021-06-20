01e29a4e665edee658b8671e22b9457ebf27cdc1
yungalyx <52767711+yungalyx@users.noreply.github.com>
2021-06-19 19:49:21 -0700

    deplyment ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
 import './App.scss';
 import { Canvas, useFrame } from '@react-three/fiber'
-import React, {useState, useRef, useEffect} from 'react'
+import React, {useState, useRef, useEffect, Suspense} from 'react'
 import Header from "./components/header"
 import { Line, OrbitControls, Html } from '@react-three/drei'
 import { Sphere } from "../src/components/Sphere"
 import axios from "axios"
 import img from "./assets/space.gif"
 
+
+
 function App() {
   // given a wallet address
   // balance determines size
 
-  const [mainAddress, setMainAddress] = useState("0x917Fb82d1f1011B4461F406897F1832072232FA6")
+  const [mainAddress, setMainAddress] = useState("0x4D25eF2DcBD733A2868f8c3cfb409b2A4099a977".toLowerCase())
   const [mainBalance, setMainBalance] = useState([])
   const [loading, setLoading] = useState(true)
+  const [transactionMap, setTransactionMap] = useState({})
+
+  function manageTransactions(data) {
+    var dict = new Map();
+    let ftm_rate = 0; // loops to collect average price from value_quote, used when value_quote is null but balance isn't.
+    data.forEach(x => {
+      if (x.value_quote != null && x.value != "0") {
+        ftm_rate = x.value_quote * Math.pow(10, 18) / x.value
+      } 
+      let myquote = (x.value_quote === null ? (x.value * ftm_rate / Math.pow(10, 18)) : x.value_quote)
+      // console.log(myquote, x.value_quote, x.value)
+      if (x.from_address === mainAddress) {
+        if (!(dict.has(x.to_address))) {
+          var other = {
+            in: 0,
+            out: myquote,
+          };
+          dict.set(x.to_address, other);
+        } else {
+          let curr = dict.get(x.to_address)
+          curr.out += myquote
+          dict.set(x.to_address, curr)
+        }
+      } else {
+        if (!(dict.has(x.from_address))) {
+          var other = {
+            in: myquote,
+            out: 0
+          };
+          dict.set(x.from_address, other);
+        } else {
+          let curr = dict.get(x.from_address)
+          curr.in += myquote
+          dict.set(x.from_address, curr)
+        }
+      }
+    })
+    let otherAddress = []
+    dict.forEach((k, v) => {
+      otherAddress.push({
+        address: v,
+        in: k.in,
+        out: k.out
+      })
+    })
+    setoOtherAddresses(otherAddress)
+   
+  }
+   
 
   useEffect(() => {
+    const getBalance = axios.get(`https://api.covalenthq.com/v1/250/address/${mainAddress}/balances_v2/?key=ckey_e1c94fe3c10248478f939aeb7b2`)
+    const getTransactions = axios.get(`https://api.covalenthq.com/v1/250/address/${mainAddress}/transactions_v2/?key=ckey_e1c94fe3c10248478f939aeb7b2`)
+    axios.all([getBalance, getTransactions]).then(data => {
+      setMainBalance(data[0].data.data.items)
+      //console.log(data[1].data.data.items)
+      // console.log(data[1].data.data.items)
+      manageTransactions(data[1].data.data.items)
+      setLoading(false)
+    })
+
+    /*
     fetch(`https://api.covalenthq.com/v1/250/address/${mainAddress}/balances_v2/?key=ckey_e1c94fe3c10248478f939aeb7b2`)
       .then(response => response.json())
       .then(data => {
@@ -26,9 +88,10 @@ function App() {
         fetch(`https://api.covalenthq.com/v1/250/address/${mainAddress}/transactions_v2/?key=ckey_e1c94fe3c10248478f939aeb7b2`)
           .then(response => response.json())
           .then(data => {
-            console.log(data)
+            // console.log(data)
           })
     })
+    */
   }, [])
 
   const [otherAddresses, setoOtherAddresses] = useState([
@@ -59,19 +122,12 @@ function App() {
   ])
 
 
-  /*
-  <line>
-            <geometry name="geometry" vertices />
-            <lineBasicMaterial color={'hotpink'} linewidth={20}/>
-          </line>
-  */
-
   const renderNeighbours = () => {
     return <group>
       {otherAddresses.map(i => {
         const [x, y, z] = [Math.floor(Math.random() * 160)-80, Math.floor(Math.random()*120)-60, Math.floor(Math.random()*80)-40]
         return <group>
-          <Sphere position={[x, y, z]} />
+          <Sphere position={[x, y, z]} address={i.address}/>
           <Line
             points={[[0, 0, 0], [x, y, z]]}
             color={'red'}
@@ -85,9 +141,9 @@ function App() {
  
   return (
     <div className="html">
-       <Header />
-      {loading ? <img src={img}/>
-      :
+      <Header />
+      {loading ? <img src={img} />
+      : 
       <Canvas colorManagement camera={{position:[0,0, 150]}}>
         <ambientLight intensity={0} />
         <directionalLight />
---
+++ src/components/line.js
@@ -0,0 +1,6 @@
+import React, { useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Line, OrbitControls, Html } from '@react-three/drei'
+
+
